356637d69726637967d4998ccd06de92add44f91
eldaJenkins <lee.eisenhardt@gmail.com>
2016-04-04 13:55:41 -0400

    simplified database

--- schema.sql
+++ schema.sql
@@ -1,43 +1,38 @@
 CREATE TABLE Media
 (
-	id		INTEGER		PRIMARY KEY,
-	title		TEXT(200)	NOT NULL,
-	author		INTEGER(1000)	NULL,
-	publisher	INTEGER(1000)	NULL,
-	isbn		TEXT(20)	NULL,
-	media_type	INTEGER(100)	NOT NULL,
-	volume		TEXT(100)	NULL,
-	pub_year	INTEGER(4)	NULL,
-	edition		TEXT(100)	NULL,
-	printing	TEXT(100)	NULL,
-
-	FOREIGN KEY (author) REFERENCES Authors(author_name),
-	FOREIGN KEY (publisher) REFERENCES Publishers(publisher_name),
-	FOREIGN KEY (media_type) REFERENCES Types(type)
+	title		TEXT		NOT NULL,
+	author		INTEGER,
+	publisher	INTEGER,
+	isbn		TEXT,
+	media_type	INTEGER		NOT NULL,
+	volume		TEXT,
+	pub_year	INTEGER,
+	edition		TEXT,
+	printing	TEXT
 );
 
 CREATE TABLE Authors
 (
-	author_name	TEXT(200)	PRIMARY KEY	NOT NULL,
+	name		TEXT		UNIQUE		NOT NULL
 );
 
 CREATE TABLE Publishers
 (
-	publisher_name	TEXT(200)	PRIMARY KEY	NOT NULL
+	name		TEXT		UNIQUE		NOT NULL
 );
 
 CREATE TABLE Types
 (
-	type		TEXT(200)	PRIMARY KEY	NOT NULL
+	type		TEXT		UNIQUE		NOT NULL
 );
 
 CREATE TABLE Tags
 (
-	tag_name	TEXT(1000)	PRIMARY KEY	NOT NULL
+	name		TEXT		UNIQUE		NOT NULL
 );
 
 CREATE TABLE Images
 (
-	image_desc	TEXT(1000)	NULL,
-	image_filename	TEXT(1000)	PRIMARY KEY	NOT NULL
+	description	TEXT,
+	filename	TEXT		NOT NULL
 );
